00426008b07f4406a3d26c87a19cdfbf2fa64b21
guanchaoguo <771668687@qq.com>
2023-08-09 16:24:00 +0800

    init

---
+++ hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+static void print_hello();
+
+void print_hello() {
+    printf("Hello\n");
+}
---
+++ main.c
@@ -0,0 +1,6 @@
+#include "hello//hello.c"
+
+int main() {
+    print_hello();
+    return 0;
+}
